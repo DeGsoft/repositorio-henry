af94cbc10ad682fee29718adae0a64e0e96d18d9
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-10-14 12:30:13 -0300

    04

--- FT-M1/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1/04-EstructuraDeDatos-I/homework/homework.js
@@ -1,13 +1,30 @@
 'use strict';
 
 // EJERCICIO 1
-function nFactorial(n) {}
+function nFactorial(n) {
+   if (n === 0) return 1;
+	return n * nFactorial (n-1); 
+}
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+   if (n === 0 || n===1) return n;
+   return nFibonacci(n-1) + nFibonacci(n-2);
+}
 
 // EJERCICIO 3
-function Queue() {}
+function Queue() {
+   this.pila = []; 
+   this.enqueue = function enqueue(e) {
+      this.pila.push(e);
+   };
+   this.dequeue = function dequeue() {
+      return this.pila.shift();
+   };
+   this.size = function () {
+      return this.pila.length;
+   };
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
