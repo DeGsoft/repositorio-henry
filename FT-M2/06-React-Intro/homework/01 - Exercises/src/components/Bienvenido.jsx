edf083202578f3b7e00fae7acc67b70f936d9687
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-11-10 11:44:09 -0300

    06

--- FT-M2/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Botones from './Botones';
 
-const studentName = "";
+const studentName = "Nacho";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+  // el código de tu componente acá= 
+  return(
+    <div>
+      <h1>soy Henry!</h1>
+      <h3>{studentName}</h3>
+      <ul>{techSkills.map((e) => <li key={e}>{e}</li>)}</ul>
+      <Botones alerts={alerts}></Botones>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
--- FT-M2/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ FT-M2/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,18 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   return <div>
+      {
+         characters.map(character =>
+            <Card
+               key = {character.name}   
+               // name = {character.name}
+               // species = {character.species}
+               // gender = {character.gender}
+               // image = {character.image}               
+               {...character}
+               onCLose = {() => window.alert('XD')}
+            />)
+      }
+   </div>;
 }
