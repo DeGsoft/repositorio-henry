9e39fdd2b79d62fe890ef5fcd38d9abc58433d0b
DeGssoft <diegoezequielguillen@gmail.com>
2022-12-12 20:29:34 -0300

    02

--- tech-interview-preparation/Ejercicios/02-SumArray/SumArray.js
+++ tech-interview-preparation/Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,37 @@
-function SumArray (arr, n) {
+function SumArray(arr, n) {
   // Your code here:
+  return resolve1(arr, n) && resolve2(arr, n) && resolve3(arr, n);
 
+  function resolve1(arr, n) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === n) return true;
+      }
+    }
+    return false;
+  }
+
+  function resolve2(arr, n) {
+    arr.sort((a, b) => a - b);
+    let min = 0,
+      max = arr.length - 1;
+    while (min < max) {
+      let sum = arr[min] + arr[max];
+      if (sum === n) return true;
+      (sum < n) ? min++ : max--;
+    }
+    return false;
+  }
+
+  function resolve3(arr, n) {
+    return arr.reduce((subsets, value) => subsets.concat(
+      subsets.map(set => [value, ...set])
+    ), [[]])
+      .filter(subsets => subsets.length === 2)
+      .map(set => set.reduce((acc, cur) => acc + cur, 0))
+      .filter(sum => sum === n).length > 0;
+  }
+  
 }
 
 module.exports = SumArray
